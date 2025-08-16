6420495a2bf97f606e5c2e82431ff6e080a6f852
XUYOUGUANLUN <108600205+XUYOUGUANLUN@users.noreply.github.com>
2025-08-16 10:51:54 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -5,11 +5,11 @@ const customerData = [
         name: "Passion",
         role: "customer",
         rooms: [
-            { day: 1, hotel: "麗寶大飯店", number: "101" },
-            { day: 2, hotel: "麗寶大飯店", number: "102" },
-            { day: 3, hotel: "日月潭大飯店", number: "201" },
-            { day: 4, hotel: "日月潭大飯店", number: "202" },
-            { day: 5, hotel: "台中大飯店", number: "301" }
+            { day: 1, hotel: "", number: "" },
+            { day: 2, hotel: "", number: "" },
+            { day: 3, hotel: "", number: "" },
+            { day: 4, hotel: "", number: "" },
+            { day: 5, hotel: "", number: "" }
         ],
         emergency: {
             name: "XXX",
@@ -20,26 +20,27 @@ const customerData = [
                 day: 1,
                 schedule: [
                     { time: "13:00-13:30", activity: "確認資料、上車" },
-                    { time: "11:00-12:00", activity: "午餐" },
-                    { time: "12:30-15:00", activity: "景點遊覽" },
-                    { time: "15:30-17:00", activity: "自由活動" },
-                    { time: "18:00-19:00", activity: "晚餐" }
+                    { time: "", activity: "" },
+                    { time: "", activity: "" },
+                    { time: "17:30-18:00", activity: "call in" },
+                    { time: "18:00-19:00", activity: "晚餐" },
+                    { time: "19:00-:20：00", activity: "回飯店休息" }
                 ]
             },
             {
                 day: 2,
                 schedule: [
-                    { time: "8:00-9:00", activity: "早餐" },
-                    { time: "9:30-12:00", activity: "登山健行" },
-                    { time: "12:30-13:30", activity: "午餐" },
-                    { time: "14:00-17:00", activity: "湖邊遊船" },
-                    { time: "18:00-19:00", activity: "晚餐" }
+                    { time: "7:00-9:00", activity: "洗漱時間and早餐時間" },
+                    { time: "9:30-12:00", activity: "" },
+                    { time: "12:30-13:30", activity: "" },
+                    { time: "14:00-17:00", activity: "" },
+                    { time: "18:00-19:00", activity: "" }
                 ]
             },
             {
                 day: 3,
                 schedule: [
-                    { time: "8:00-9:00", activity: "早餐" },
+                    { time: "7:00-9:00", activity: "洗漱時間and早餐時間"  },
                     { time: "9:30-12:00", activity: "文化博物館參觀" },
                     { time: "12:30-13:30", activity: "午餐" },
                     { time: "14:00-16:00", activity: "手作工坊" },
@@ -50,7 +51,7 @@ const customerData = [
             {
                 day: 4,
                 schedule: [
-                    { time: "8:00-9:00", activity: "早餐" },
+                    {  time: "7:00-9:00", activity: "洗漱時間and早餐時間" },
                     { time: "9:30-12:00", activity: "參觀植物園" },
                     { time: "12:30-13:30", activity: "午餐" },
                     { time: "14:00-17:00", activity: "手作體驗" },
@@ -60,7 +61,7 @@ const customerData = [
             {
                 day: 5,
                 schedule: [
-                    { time: "8:00-9:00", activity: "早餐" },
+                    {  time: "7:00-9:00", activity: "洗漱時間and早餐時間"  },
                     { time: "9:30-11:30", activity: "紀念品採購" },
                     { time: "12:00-13:00", activity: "午餐" },
                     { time: "13:30-15:30", activity: "返回集合點" }
@@ -79,15 +80,15 @@ const leaderInfo = {
 
 // 預設登入帳號
 const users = [
-    { username: "leader1", password: "leader123", role: "leader" },
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
-    { username: "Passion", password: "00000", role: "customer" }, // 顧客預設
+    { username: "Allan", password: "950520", role: "leader" },     //領隊
+    { username: "Passion", password: "00000", role: "customer" },
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
+    { username: "", password: "00000", role: "customer" }, 
 ];
